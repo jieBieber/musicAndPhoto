dcbd76412e1212925f154836400f481c3ff2c895
zxj <205589012@qq.com>
2022-12-13 20:40:27 +0800

    修改暂停后再次播放时从头开始播放的问题

--- inc/event.h
+++ inc/event.h
@@ -8,6 +8,17 @@
 #include <unistd.h>
 #include <linux/input.h>
 
+/*
+ *  get_inputevent: 用来获取触摸屏输入事件
+ *  arg：
+ *      @start_x：指向一个int型空间，用来保存按下时触摸点的X坐标
+ *      @start_y：指向一个int型空间，用来保存按下时触摸点的Y坐标
+ *      @end_x：指向一个int型空间，用来保存松开时触摸点的X坐标
+ *      @end_y：指向一个int型空间，用来保存松开时触摸点的Y坐标
+ *  return：
+ *      成功返回0
+ *      失败返回-1
+**/
 int get_inputevent(int*start_x,int*start_y,int*end_x,int*end_y);
 
 int key();
--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include "event.h"
 #include "music.h"
 #include "stdbool.h"
+#include <wait.h>
 
 void my_signal(int signum)
 {
@@ -25,7 +26,7 @@ int main(void)
     char*bmp[] = {"1.bmp","2.bmp","3.bmp","4.bmp","5.bmp","6.bmp","7.bmp",
                   "8.bmp","9.bmp","10.bmp","11.bmp","12.bmp"};
     pid_t  pid = 0;
-    int i = 0,j = 0;
+    int i = 0,j = 0,k = 0;
 	char *mp3_name[] = {"1.mp3","2.mp3"};
 
     //记录菜单等级
@@ -33,7 +34,9 @@ int main(void)
     //记录音乐播放的状态
     int pictrueState = 1;
     //记录纯音乐播放的状态
-    bool musicState = true;
+    int musicState = -1,flag = 0;
+    //记录是否已经开始自动播放图片
+    bool autoPicture = false;
 
 
     /* 捕捉了SIGINT信号（Ctrl+C），当用户按下 Ctrl+C结束父进程时，会自动去执行 my_signal 函数 */
@@ -47,72 +50,85 @@ int main(void)
     /*增加触摸屏控制*/
     while(1)
     {
-            get_inputevent(&start_x,&start_y,&end_x,&end_y);
-
-            printf("start_x:%d,start_y:%d,end_x:%d,end_y:%d\n",start_x,start_y,end_x,end_y);
-
-            //向上滑动
-            if(start_y-end_y>50)
-            {
-                printf("向上滑动 照片播放\n");
-                menuState = 0;
-                lcd_draw_clear(0,0,480,800,0xffffff);
-                bmp_show("picture.bmp",269,184);
-                continue;
-            }
-            //向下滑动
-            else if (end_y-start_y>50)
-            {
-                printf("向下滑动  音乐播放\n");
-                menuState = 1;
-                lcd_draw_clear(0,0,480,800,0xffffff);
-                showMenu(0);
-                stopMusic();
-                continue;
-            }
-            
-            printf("menuState:%d\n",menuState);
-#if 1
+        get_inputevent(&start_x,&start_y,&end_x,&end_y);
+        //从左上往右下滑动
+        if(end_y-start_y>50 && end_x-start_x>50 && menuState!=-1)
+        {
+            menuState = -1;
+            stopMusic();
+            lcd_draw_clear(0,0,480,800,0xffffff);
+            bmp_show("home.bmp",30,168);
+            continue;
+        }
+        //从右上往左下滑动
+        else if(end_y-start_y>50 && start_x-end_x>50 && menuState!=2)
+        {
+            stopMusic();
+            menuState = 2;
+            continue;
+        }
+        //向上滑动
+        else if(abs(end_x-start_x)<50 && start_y-end_y>50 && menuState!=0)
+        {
+            menuState = 0;
+            stopMusic();
+            lcd_draw_clear(0,0,480,800,0xffffff);
+            bmp_show("picture.bmp",269,184);
+            continue;
+        }
+        //向下滑动
+        else if (abs(end_x-start_x)<50 && end_y-start_y>50 && menuState!=1)
+        {
+            menuState = 1;
+            musicState = -1;
+            lcd_draw_clear(0,0,480,800,0xffffff);
+            showMenu(0);
+            stopMusic();
+            continue;
+        }
+        
+        //图片播放
         if(menuState == 0)
         {
             if(end_x-start_x > 50)
             {
-                printf("切换到下一张照片\n");
-                pid = fork();
                 stopMusic();
+                pid = fork();
                 if(pid>0)
                 {
-                    if(++i>4)
-                        i=0;
+                    if(++k>4)
+                        k=0;
                     lcd_draw_clear(0,0,480,800,0xffffff);
-                    bmp_show(bmp[i],0,0);
+                    bmp_show(bmp[k],0,0);
                 }
                 else if(pid == 0)
-                    playMusic(mp3_name[i%2]);
+                {
+                    playMusic(mp3_name[k%2]);
+                    kill(pid,SIGKILL);
+                }
             }
             else if(start_x-end_x > 50)
             {
-                printf("切换到上一张照片\n");
-                pid = fork();
                 stopMusic();
+                pid = fork();
                 if(pid>0)
                 {
-                    if(--i<0)
-                        i=4;
+                    if(--k<0)
+                        k=4;
                     lcd_draw_clear(0,0,480,800,0x000000);
-                    bmp_show(bmp[i],0,0);
+                    bmp_show(bmp[k],0,0);
                 }
                 else if(pid == 0)
-                    playMusic(mp3_name[i%2]);
+                {
+                    playMusic(mp3_name[k%2]);
+                    kill(pid,SIGKILL);
+                }
             }
             else if((end_x>200 && end_x < 600) || (end_y>200 && end_y < 400))
             {
-                printf("停止播放音乐开始自动播放照片\n");
-                // pid = fork();
-                // count++;
-                // if(pid == 0 && count%2 == 0)
-                // {
-                    printf("开始幻灯片播放\n");
+                pid = fork();
+                if(pid == 0)
+                {
                     stopMusic();
                     for(j=0;j<12;j++)
                     {
@@ -120,55 +136,56 @@ int main(void)
                         bmp_show(bmp[j],0,0);
                         sleep(1);
                     }
-                // }
-                // else if(pid== 0 && count%2 == 1)
-                // {
-                //     printf("结束幻灯片播放\n");
-                // }
+                    kill(pid,SIGKILL);
+                }
             }
         }
-        else if(menuState == 1)
-#endif
 
-#if 1
+        //音乐播放
+        else if(menuState == 1)
         {
             int temp = getMenuValueByLocation(start_x,start_y,end_x,end_y);
-            printf("getMenuValueByLocation:%d",temp);  
-            printf("   i:%d   j:%d musicState:%d\n",i,j,musicState);
             //1表示点击了开始按钮
-            if(temp==1 && musicState)
+            if(temp==1 && musicState==-1)
             {
-                printf("开始放歌\n");
-                musicState = false;
+                //第一次放歌
+                musicState = 1;
                 showMenu(1);
                 pid = fork();
                 if(pid == 0)
                 {
-                    printf("bofang\n");
-                    playMusic(mp3_name[i]);
+                    playMusic(mp3_name[i++%2]);
                     kill(pid,SIGKILL);
                 }
             }
             //2表示点击暂停
-            else if(temp == 1 && musicState==false)
+            else if(temp == 1 && musicState!=-1)
             {
-                printf("停止放歌\n");
-                musicState = true;
-                showMenu(0);
-                stopMusic();
+                //放歌已经暂停 接下来应该是继续上一首 而不是重新开始
+                if(musicState%2 == 0)
+                {
+                    showMenu(1);
+                    system("killall -CONT madplay");
+                }
+                //还未放歌 应该点击开始放歌
+                else
+                {
+                    showMenu(0);
+                    system("killall -STOP madplay");
+                }
+                musicState ^= 1;
             }
             //3表示点击切换上一首
             else if(temp == 2)
             {
-                printf("切换上一首\n");
                 showMenu(1);
+                musicState ^= 1;
                 pid = fork();
                 if(--i == -1)
                     i = 1;
                 if(pid == 0)
                 {
                     stopMusic(); 
-                    printf("i:%d\n",i);
                     playMusic(mp3_name[i]);
                     kill(pid,SIGKILL);
                 }
@@ -176,27 +193,29 @@ int main(void)
             //4表示切换下一首
             else if(temp == 3)
             {
-                printf("切换下一首\n"); 
                 showMenu(1);
+                musicState ^= 1;
                 pid = fork();
                 if(++i == 2)
                     i = 0;
                 if(pid == 0)
                 {
                     stopMusic(); 
-                    printf("i:%d\n",i);
                     playMusic(mp3_name[i%2]); 
                     kill(pid,SIGKILL);
                 }
             }
-            //发生错误
+            //无效值
             else
             {
                 printf("无效值\n");
             }
         }
-        
-#endif
+        //视频播放
+        else if(menuState == 1)
+        {
+            printf("播放视频\n");
+        }
     }
     //3.解除屏幕，不再需要LCD显示时，解除屏幕    
     lcd_uninit(); //解除屏幕
